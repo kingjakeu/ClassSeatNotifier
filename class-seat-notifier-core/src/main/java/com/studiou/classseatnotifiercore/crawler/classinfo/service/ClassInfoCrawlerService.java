362c6db5b621d467c63072946bdadda6c8c8437f
kingjake <jakeyoo12@gmail.com>
2019-08-17 18:25:02 +0900

    Delete old codes

--- class-seat-notifier-core/src/main/java/com/studiou/classseatnotifiercore/crawler/classinfo/service/ClassInfoCrawlerService.java
+++
@@ -1,7 +0,0 @@
-package com.studiou.classseatnotifiercore.crawler.classinfo.service;
-
-public interface ClassInfoCrawlerService {
-    void searchClassInfoScheduler();
-    void getClassInfoFromSource();
-    void updateClassInfo();
-}
